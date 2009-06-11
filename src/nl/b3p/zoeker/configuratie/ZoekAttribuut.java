4d6884f5e077dca05810d825fe4b13cf606a0bb1
Roy Braam <braam.roy@gmail.com>
2009-06-11 19:15:16 +0000

    Vergeten bestand: volgorde goed gezet in hbm file.

--- src/nl/b3p/zoeker/configuratie/ZoekAttribuut.java
+++ src/nl/b3p/zoeker/configuratie/ZoekAttribuut.java
@@ -10,19 +10,7 @@ package nl.b3p.zoeker.configuratie;
  * @author Roy
  */
 public class ZoekAttribuut extends Attribuut{
-    private Integer volgorde;
+    
 
-    /**
-     * @return the volgorde
-     */
-    public Integer getVolgorde() {
-        return volgorde;
-    }
-
-    /**
-     * @param volgorde the volgorde to set
-     */
-    public void setVolgorde(Integer volgorde) {
-        this.volgorde = volgorde;
-    }
+   
 }
