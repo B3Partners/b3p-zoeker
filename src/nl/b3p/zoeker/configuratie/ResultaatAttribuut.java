93f0f98ffbc16c4063e5fe1cf99c9e01b94dd300
Roy Braam <braam.roy@gmail.com>
2010-02-09 09:00:57 +0000

    Default constr. aangemaakt voor hiberante.

--- src/nl/b3p/zoeker/configuratie/ResultaatAttribuut.java
+++ src/nl/b3p/zoeker/configuratie/ResultaatAttribuut.java
@@ -11,6 +11,9 @@ package nl.b3p.zoeker.configuratie;
  */
 public class ResultaatAttribuut extends Attribuut{
 
+    public ResultaatAttribuut(){
+        
+    }
     public ResultaatAttribuut(Integer id, String naam, String attribuutnaam, String label, Integer type, Integer volgorde) {
         super(id,naam,attribuutnaam,label,type,volgorde);
     }
--- src/nl/b3p/zoeker/configuratie/ZoekAttribuut.java
+++ src/nl/b3p/zoeker/configuratie/ZoekAttribuut.java
@@ -11,6 +11,9 @@ package nl.b3p.zoeker.configuratie;
  */
 public class ZoekAttribuut extends Attribuut{
 
+    public ZoekAttribuut(){
+        
+    }
     public ZoekAttribuut(Integer id, String naam, String attribuutnaam, String label, Integer type, Integer volgorde) {
         super(id,naam,attribuutnaam,label,type,volgorde);
     }
