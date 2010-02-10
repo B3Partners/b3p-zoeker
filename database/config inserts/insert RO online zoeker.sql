03a9a29257cb375024ca15c1c1033b41d391ff2f
Roy Braam <braam.roy@gmail.com>
2010-02-10 14:59:34 +0000

    Naam toegevoegd aan zoekconfiguratie inserts

--- database/config inserts/insert RO online zoeker.sql
+++ database/config inserts/insert RO online zoeker.sql
@@ -1,6 +1,6 @@
 INSERT INTO bron (id,naam, url, volgorde, gebruikersnaam, wachtwoord) VALUES (1,'nlrpp', 'http://afnemers.ruimtelijkeplannen.nl/afnemers/services?Version=1.0.0', 1, NULL, NULL);
 
-INSERT INTO zoekconfiguratie (id,naam, featuretype, parentbron, parentzoekconfiguratie) VALUES (1,'', 'app:Plangebied', 1, NULL);
+INSERT INTO zoekconfiguratie (id,naam, featuretype, parentbron, parentzoekconfiguratie) VALUES (1,'Ro-online zoeker', 'app:Plangebied', 1, NULL);
 
 INSERT INTO resultaatveld (label, attribuutnaam, type, zoekconfiguratie, naam, volgorde) VALUES ('plan naam', 'naam', 2, 1, 'plannaam', NULL);
 INSERT INTO resultaatveld (label, attribuutnaam, type, zoekconfiguratie, naam, volgorde) VALUES ('plan id', 'identificatie', 1, 1, 'planid', NULL);
@@ -12,7 +12,7 @@ INSERT INTO resultaatveld (label, attribuutnaam, type, zoekconfiguratie, naam, v
 INSERT INTO zoekveld (label, attribuutnaam, zoekconfiguratie, type, volgorde, naam) VALUES ('Plannen', 'app:overheidscode', 1, NULL, NULL, 'plannen');
 
 INSERT INTO bron (id, naam, url, volgorde, gebruikersnaam, wachtwoord) VALUES (2, 'local', 'jdbc:postgresql://vulhierjehostin:5432/vulhierjedatabasein.ennadepunthetschema', 2, 'vulhierdeusernamein', 'vulhierhetwachtwoordin');
-INSERT INTO zoekconfiguratie (id, naam, featuretype, parentbron, parentzoekconfiguratie) VALUES (3, '', 'planeigenaar', 2, NULL);
+INSERT INTO zoekconfiguratie (id, naam, featuretype, parentbron, parentzoekconfiguratie) VALUES (3, 'Zoek eigenaar', 'planeigenaar', 2, NULL);
 INSERT INTO resultaatveld (label, attribuutnaam, type, zoekconfiguratie, naam, volgorde) VALUES ('code', 'code', 1, 3, 'code', NULL);
 INSERT INTO resultaatveld (label, attribuutnaam, type, zoekconfiguratie, naam, volgorde) VALUES ('naam', 'naam', 2, 3, 'naam', NULL);
 INSERT INTO zoekveld (label, attribuutnaam, zoekconfiguratie, type, volgorde, naam) VALUES ('Eigenaar', 'naam', 3, NULL, NULL, 'eigenaar');
